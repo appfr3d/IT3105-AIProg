8f3d666aedd8ffed7731657fe7953c0ee963568f
Alfred Lieth Årøe <alfredla@stud.ntnu.no>
2021-03-26 14:12:55 +0100

    save last model

--- project_2/ReinforcementLearner.py
+++ project_2/ReinforcementLearner.py
@@ -64,8 +64,8 @@ class ReinforcementLearner():
       # self.actor.epsilon_decay()
 
       # If we are on save interval
-      if episode % int(math.floor(self.config.number_of_episodes / self.config.model_count)) == 0:
-        self.actor.save(self.save_path, episode)
+      if (episode + 1) % int(math.floor(self.config.number_of_episodes / self.config.model_count)) == 0:
+        self.actor.save(self.save_path, (episode + 1))
 
       # Logaritmic
       # if self.peg_log[-1] == 1:
